9f04b8807f1baacbd4af15468393ecc4e4e12a3e
Subhankar-Panda-india <hello.subhankar.digital@gmail.com>
2026-08-26 19:27:47 -0800

    view theater map

--- C/practiceprojects/bookingsystem/engine.c
+++ C/practiceprojects/bookingsystem/engine.c
@@ -1,27 +1,48 @@
 #include<stdio.h>
+int mainmenu(){
+    int userinput;
+    printf("MENU:\nPress1:View Theater Map\nPress2:Book a Seat\nPress3:Exit Application\n");
+    if(scanf("%d",&userinput)!=1||userinput<=0 || userinput >3){
+        while (getchar() != '\n');
+        mainmenu();
+    }
+    return userinput;
+}
+
+void viewtheatermap(){
+    int seats[3][4];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            seats[i][j]=0;
+        }
+    }
+    int isbooked=1;
+    //some added bookings
+    seats[2][2]=isbooked;
+    seats[1][2]=isbooked;
+    seats[2][3]=isbooked;
+    seats[0][3]=isbooked;
+    printf("\n\n");
+    printf("SHOWING THEATER MAP\n");
+    for(int k=0;k<3;k++){
+        for(int l=0;l<4;l++){
+            (seats[k][l]==0)?printf("[o] "):printf("[x] ");
+        }printf("\n");
+    }
+}
+
+
 
 int main (void){
     printf("WELLCOME TO TICKET BOOKING SYSTEM\n");
+
+    int userinputresult=mainmenu();
     
-    printf("MENU:\nPress1:View Theater Map\nPress2:Book a Seat\nPress3:Exit Application\n");
-    
-    
-    // int seats[3][4];
-    // for(int i=0;i<3;i++){
-    //     for(int j=0;j<4;j++){
-    //         seats[i][j]=0;
-    //     }
-    // }
-    // int isbooked=1;
-    // //some added bookings
-    // seats[2][2]=isbooked;
-    // seats[1][2]=isbooked;
-    // seats[2][3]=isbooked;
-    // seats[0][3]=isbooked;
-    // for(int k=0;k<3;k++){
-    //     for(int l=0;l<4;l++){
-    //         (seats[k][l]==0)?printf("[o] "):printf("[x] ");
-    //     }printf("\n");
-    // }
+    switch(userinputresult){
+        case 1: viewtheatermap();break;
+        // case 2: bookticket();break;
+        case 3: printf("\n\nExiting the Application!\n");break;
+    }
+        
     return 0;
 }
