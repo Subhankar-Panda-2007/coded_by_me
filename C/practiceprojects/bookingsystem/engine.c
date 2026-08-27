6887804a2c88e1309048b8569e47a6450cc8900a
Subhankar-Panda-india <hello.subhankar.digital@gmail.com>
2026-08-26 18:28:01 -0800

    folder name change

---
+++ C/practiceprojects/bookingsystem/engine.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main (void){
+    printf("WELLCOME TO TICKET BOOKING SYSTEM\n");
+    
+    printf("MENU:\nPress1:View Theater Map\nPress2:Book a Seat\nPress3:Exit Application\n");
+    
+    
+    // int seats[3][4];
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<4;j++){
+    //         seats[i][j]=0;
+    //     }
+    // }
+    // int isbooked=1;
+    // //some added bookings
+    // seats[2][2]=isbooked;
+    // seats[1][2]=isbooked;
+    // seats[2][3]=isbooked;
+    // seats[0][3]=isbooked;
+    // for(int k=0;k<3;k++){
+    //     for(int l=0;l<4;l++){
+    //         (seats[k][l]==0)?printf("[o] "):printf("[x] ");
+    //     }printf("\n");
+    // }
+    return 0;
+}
